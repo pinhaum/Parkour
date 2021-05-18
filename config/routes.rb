6219ae4e435e1f3b8fc476271ee86e8d4e8539e6
Gabriel Ramos <gcrepho@gmail.com>
2021-05-18 18:57:59 -0300

    add resources for funcionario

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'funcionario/index'
-  get 'funcionario/show'
-  get 'funcionario/new'
-  get 'funcionario/edit'
   resources :clientes
+  resources :funcionarios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
